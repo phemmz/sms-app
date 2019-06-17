88065d6c95c165a14afd4a25963566a1c10c25a3
Adetunji Femi <phemmzmcllroy@gmail.com>
2019-06-17 05:55:39 +0100

    feat: add endpoints for getting all contacts and getting contacts by phonenumber

--- src/server/controllers/contacts.js
+++ src/server/controllers/contacts.js
@@ -12,7 +12,6 @@ const createContact = async (request, response) => {
       });
     } else {
       response.status(409).json({
-        contact,
         success: false,
         message: 'Contact already exist',
       });
@@ -25,6 +24,73 @@ const createContact = async (request, response) => {
   }
 };
 
+const getAllContacts = async (_, response) => {
+  try {
+    const contacts = await Contact.findAll();
+
+    if (contacts.length) {
+      response.status(200).json({
+        contacts,
+        success: true,
+        message: 'Contacts retrieved',
+      });
+    } else {
+      response.status(404).json({
+        contacts,
+        success: true,
+        message: 'No contact found',
+      });
+    }
+  } catch(error) {
+    response.status(500).json({
+      error,
+      success: false,
+      message: 'Failed to get all contacts',
+    });
+  }
+}
+
+const getContactByPhoneNumber = async (request, response) => {
+  try {
+    const { phoneNumber } = request.params;
+
+    if (phoneNumber.length !== 11) {
+      return response.status(422).json({
+        success: false,
+        message: 'Phone number must be 11 digits'
+      });
+    } 
+
+    const contact = await Contact.findOne({
+      where: {
+        phoneNumber
+      }
+    });
+
+    if (contact === null) {
+      return response.status(404).json({
+        contact,
+        success: true,
+        message: 'Contact not found',
+      });
+    }
+
+    return response.status(200).json({
+      contact,
+      success: true,
+      message: 'Contact retrieved',
+    });
+  } catch(error) {
+    response.status(500).json({
+      error,
+      success: false,
+      message: 'Failed to get contact',
+    });
+  }
+}
+
 export {
   createContact,
+  getAllContacts,
+  getContactByPhoneNumber
 };
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,12 +1,18 @@
 import express from 'express';
 
-import { createContact } from './controllers/contacts';
+import {
+  createContact,
+  getAllContacts,
+  getContactByPhoneNumber
+} from './controllers/contacts';
 import { sendMessage } from './controllers/messages';
 import { validateNewContact } from './helpers/contactValidations';
 import { validateMessage } from './helpers/messageValidations';
 
 const router = express.Router();
 
+router.get('/contacts', getAllContacts);
+router.get('/contacts/:phoneNumber', getContactByPhoneNumber);
 router.post('/contact', validateNewContact, createContact);
 router.post('/message', validateMessage, sendMessage);
 
